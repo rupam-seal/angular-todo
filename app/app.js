a6f1ae177a4ef2deeb21f90c16443bd001011eb4
rupam-seal <rupam.x.seal@gmail.com>
2023-05-07 19:13:45 +0530

    ✨feat(*): add all files

--- app/app.js
+++ app/app.js
@@ -1,58 +1,79 @@
-const myNinjaApp = angular.module("myNinjaApp", [])
+  const myNinjaApp = angular.module("myNinjaApp", ["ngRoute", 'ngAnimate']);
 
-myNinjaApp.controller("NinjaController", ['$scope', function($scope){
-  // $scope.message = "hey, all"
-  // $scope.ninjas = ["yoshi", "crystal", "ryu", "shaun"]
-  $scope.removeNinja = function(ninja) {
-    var removeNinja = $scope.ninjas.indexOf(ninja);
-    $scope.ninjas.splice(removeNinja, 1);
-  }
+  myNinjaApp.config(["$routeProvider", function($routeProvider){
+    $routeProvider
+      .when('/home', {
+        templateUrl: './views/home.html',
+        controller: 'NinjaController'
+      })
+      .when('/directory', {
+        templateUrl: './views/directory.html',
+        controller: 'NinjaController'
+      })
+      .when('/contact', {
+        templateUrl: './views/contact.html',
+        controller: 'ContactController'
+      })
+      .when('/contact-success', {
+        templateUrl: './views/contact-success.html',
+        controller: 'ContactController'
+      })
+      .otherwise({
+        redirectTo: '/home'
+      })
+  }])
 
-  $scope.addNinja = function() {
-    $scope.ninjas.push(
-      {
-        name: $scope.newninja.name,
-        belt: $scope.newninja.belt,
-        rate: parseInt($scope.newninja.rate),
-        available: true,
-        thumb: "./content/img/pic" + ($scope.ninjas.length + 1) + ".jpg",
+  myNinjaApp.directive("randomNinja", [function(){
+    return {
+      restrict: 'E',
+      scope: {
+        ninjas: '=',
+        title: '='
+      },
+      templateUrl: './views/randomNinja.html',
+      transclude: true,
+      replace: true,
+      controller: function($scope){
+        $scope.random = Math.floor(Math.random() * 4)
       }
-    )
-    $scope.newninja.name = '';
-    $scope.newninja.belt = '';
-    $scope.newninja.rate = '';
-  }
+    };
+  }])
 
-  $scope.ninjas = [
-    {
-      name: "yoshi",
-      belt: "green",
-      rate: 50,
-      available: true,
-      thumb: "./content/img/pic1.jpg"
-    },
-    {
-      name: "crystal",
-      belt: "yellow",
-      rate: 30,
-      available: false,
-      thumb: "./content/img/pic2.jpg"
-    },
-    {
-      name: "ryu",
-      belt: "orange",
-      rate: 10,
-      available: true,
-      thumb: "./content/img/pic3.jpg"
-    },
-    {
-      name: "shaun",
-      belt: "black",
-      rate: 100,
-      available: true,
-      thumb: "./content/img/pic4.jpg"
-    },
-  ]
+  myNinjaApp.controller("NinjaController", ['$scope', '$http', function($scope, $http){
+    $scope.removeNinja = function(ninja) {
+      var removeNinja = $scope.ninjas.indexOf(ninja);
+      $scope.ninjas.splice(removeNinja, 1);
+    }
+
+    $scope.addNinja = function() {
+      $scope.ninjas.push(
+        {
+          name: $scope.newninja.name,
+          belt: $scope.newninja.belt,
+          rate: parseInt($scope.newninja.rate),
+          available: true,
+          thumb: "./content/img/pic" + ($scope.ninjas.length + 1) + ".jpg",
+        }
+      )
+      $scope.newninja.name = '';
+      $scope.newninja.belt = '';
+      $scope.newninja.rate = '';
+    }
+
+    $scope.removeAll = function() {
+      $scope.ninjas = []; 
+    }
+
+    $http.get('./data/ninjas.json').then(function(response){
+        $scope.ninjas = response.data
+    })
+
+}])
+
+myNinjaApp.controller("ContactController", ['$scope', '$location', function($scope, $location){
+  $scope.sendMessage = function() {
+    $location.path('/contact-success');
+  }
 }])
 
 
